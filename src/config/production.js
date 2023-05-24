97f8ea7b31be6ffb7a25d172b68bef8709aecd94
Elizabeth Salako <waleade602@gmail.com>
2023-05-24 08:56:32 -0500

    added config for otp auth

--- src/config/production.js
+++ src/config/production.js
@@ -4,6 +4,9 @@ dotenv.config();
 export const production = {
   mongodb_connection_url: process.env.PROD_DATABASE_URL,
   port: +process.env.PROD_PORT,
-  bcrypt_salt_round: process.env.PROD_BCRYPT_SALT_ROUND,
-  jwt_web_token: process.env.PROD_JWT_SECRET
+  bcrypt_password_salt_round: process.env.PROD_PASSWORD_BCRYPT_SALT_ROUND,
+  bcrypt_OTP_salt_round: process.env.PROD_OTP_BCRYPT_SALT_ROUND,
+  jwt_web_token: process.env.PROD_JWT_SECRET,
+  mail_trap_username: process.env.PROD_MAIL_TRAP_USERNAME,
+  mail_trap_password: process.env.PROD_MAIL_TRAP_PASSWORD
 }

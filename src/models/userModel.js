8e77da76678fd2e90afdd9c30979bd668baeb415
Elizabeth Salako <waleade602@gmail.com>
2023-05-24 01:59:36 -0500

    mongoose user model schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,36 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    immutable: true,
+    validators: {
+      match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, "Please add a valid email string to the email path."]
+    }
+  },
+  phoneNumber: {
+    type: Number,
+    required: [true, 'Please enter phone number digits'],
+    validators: {
+      match: [/^\\d{ 8, 11}$/, 'Please add a valid phone number with 10 or 11 digits']
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePic: {
+    type: String,
+    default: ""
+  }
+
+})
+
+export const userModel = mongoose.model('User', userSchema);

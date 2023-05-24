977047ca46ccc6996fac606701e3fc7b497a66a0
Elizabeth Salako <waleade602@gmail.com>
2023-05-24 01:56:53 -0500

    mounted routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import morgan from "morgan";
 import { globalErrorHandler } from "./src/utils/globalError.js";
 import { config } from "./src/config/index.js";
+import { userRouter } from "./src/routes/UserRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -13,10 +14,14 @@ const port = config.port;
 app.use(express.json())
 app.use(morgan('tiny'))
 
+// Mounting router
+app.use('/api/v1/user', userRouter);
+
 // Database connection
 mongoose.connect(config.mongodb_connection_url)
   .then(() => console.log('Database connection successful'))
   .catch((err) => console.log(err.message));
+
 
 // globalErrorHandler middle-ware
 app.use(globalErrorHandler)

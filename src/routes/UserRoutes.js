c895f1f6fa4e514d35ae329b5189367b595fe720
Elizabeth Salako <waleade602@gmail.com>
2023-05-24 09:00:06 -0500

    created a verify email route

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createAccount, emailLogin } from '../controllers/userControllers.js';
+import { createAccount, emailLogin, verifyEmail } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 // user registration or create account
 router.post('/create-account', createAccount);
+router.post('/verify-email', verifyEmail);
 router.post('/email-login', emailLogin);
+
 
 export { router as userRouter };

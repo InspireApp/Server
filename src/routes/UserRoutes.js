15fca7aca7a868185e5c30cac92be51ee2dec7f7
Elizabeth Salako <waleade602@gmail.com>
2023-05-24 02:00:11 -0500

    user routes

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,3 +1,10 @@
 import express from 'express';
+import { createAccount, emailLogin } from '../controllers/userControllers.js';
 
 const router = express.Router();
+
+// user registration or create account
+router.post('/create-account', createAccount);
+router.post('/email-login', emailLogin);
+
+export { router as userRouter };

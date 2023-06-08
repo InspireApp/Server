4eee991d79ca5f28e76c842bd58e91b4b824e8e8
Elizabeth Salako <waleade602@gmail.com>
2023-06-08 11:25:58 -0500

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import cors from 'cors';
 import express from "express";
 import mongoose from 'mongoose';
 import morgan from "morgan";
@@ -18,6 +19,7 @@ const port = config.port;
 // global middle-wares
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors());
 
 // call passport file
 passportSetup();
@@ -37,6 +39,7 @@ app.use(passport.session())
 
 
 // Mounting router
+app.use('/', userAuthRouter)
 app.use('/api/v1/auth', userAuthRouter);
 app.use('/api/v1/profile', userProfileRouter);
 
